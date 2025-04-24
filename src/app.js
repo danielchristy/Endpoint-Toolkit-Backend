cdf157da7907c92a7191ecfbb52bffc8da900cf0
Daniel Christy <dchristy@basecampcodingacademy.org>
2025-04-24 18:25:46 -0500

    test

--- src/app.js
+++ src/app.js
@@ -11,22 +11,19 @@ import userRecommendRoutes from "../routes/userRecommendRoutes.js";
 
 dotenv.config();
 
-const APP_URI = process.env.MONGO_URI;
-const APP_PORT = process.env.PORT;
+const MONGO = process.env.MONGO_URI;
 
 const allowedOrigins = [
     'https://devwaypoint.xyz',
-    'http://localhost:3000'
+    'http://localhost:3000',
 ];
 
 const main = async () => {
     const app = express();
-    const port = APP_PORT;
     try {
-        await mongoose.connect(APP_URI);
-        console.log("Connected to MongoDB");
+        await mongoose.connect(MONGO);
     } catch (error) {
-        console.error("Error connecting to MongoDB:", error);
+        console.error("Error connecting to database:", error);
         process.exit(1);
     }
 
@@ -38,10 +35,6 @@ const main = async () => {
     app.use("/api/careers", careerRoutes);
     app.use("/api/recommendations", userRecommendRoutes);
     app.use(errorHandler);
-
-    app.listen(port, () => {
-        console.log(`Server is running on port ${port}`);
-    });
 };
 
 main();
